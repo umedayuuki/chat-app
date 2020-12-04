e347bc010cbab950ea35e694b4dd8b3f5cfb75a6
umedayuuki <ume1017da@icloud.com>
2020-12-04 22:48:55 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,13 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+  end
+
 end
